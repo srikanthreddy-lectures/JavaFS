331ba50c24bd2935474f114a6695c839b47eafb1
srikanthreddy-lectures <83769934+srikanthreddy-lectures@users.noreply.github.com>
2021-05-22 15:58:42 +0530

    Update SQLDemo.sql

--- SQLDemo.sql
+++ SQLDemo.sql
@@ -1,10 +1,23 @@
-create table employee(
-  empId int,
-  empName varchar(100),
-  empAddress text
+create table job(
+  job_id int primary key,
+  job_title varchar(100),
+  salary decimal(6,0)
 );
 
-alter table employee add empDesg varchar(100);
-alter table employee modify empId int primary key;
-alter table employee drop column empDesg;
+
+create table employee(
+  empId int primary key,
+  empName varchar(100),
+  empAddress text,
+  job_id int,
+  foreign key(job_id) references job(job_id)
+);
+
+//alter table employee add empDesg varchar(100);
+//alter table employee modify empId int primary key;
+//alter table employee drop column empDesg;
+
+drop table job;
+
+desc job;
 desc employee;
